7c27e41a4dd33dd462f2e3f910ccad736427150d
Hugo V <hugopcveloso@gmail.com>
2021-04-02 18:39:04 +0100

    initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,42 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: 'blue'
   },
   mutations: {
+    decreaseCounter(state, randomNumber) {
+      state.counter -= randomNumber
+    },
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber
+    },
+    setColorCode(state, newValue) {
+      state.colorCode = newValue
+    }
   },
   actions: {
+    increaseCounter({commit}) {
+      axios.get('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then((res)=> commit('increaseCounter', res.data))
+      .catch((err) => alert(err))
+    },
+    decreaseCounter({commit}) {
+      axios.get('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+      .then((res)=> commit('decreaseCounter', res.data))
+      .catch((err) => {alert(err)})
+    },
+    setColorCode({commit}, newValue) {
+      commit('setColorCode', newValue)
+    }
+  },
+  getters: {
+    counterSquared(state) {
+      return state.counter * state.counter
+    }
+
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Counter />
+
+    <CounterSquared />
+    <Buttons />
+    <ColorCode />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Counter from "../components/Counter";
+import CounterSquared from "../components/CounterSquared";
+import Buttons from "../components/Buttons";
+import ColorCode from "../components/ColorCode";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Counter,
+    CounterSquared,
+    Buttons,
+    ColorCode,
+  },
+};
 </script>
+
+<style lang="sass" >
+div
+  margin-bottom: 10px
+
+.counter
+  font-size: 80px
+
+.buttons
+  button
+    color: black
+    font-size: 40px
+    width: 100px
+    margin: 0 10px
+</style>
